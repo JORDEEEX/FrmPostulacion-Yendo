a93170541db7cd2810c138ab97cfdd31cff441f2
gomez <ale.mcec@gmail.com>
2021-10-02 01:55:15 -0500

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,7 +14,7 @@ admin.initializeApp({
 const db = admin.firestore();
 const storage = admin.storage();
 router.get('/', (req, res) => {
-    res.render('confirmation');
+    res.render('index');
 })
 
 
@@ -34,9 +34,11 @@ router.post( '/new-employee', async (req, res) => {
     // db.collection("employees").doc().set(data); // db 
     
     // console.log(results)
-    res.send('received');
+    res.redirect('/succesful');
 })
-
+router.get('/succesful', (req, res) => {
+    res.render('confirmation');
+})
 module.exports = router;
 
     // textIbm = 'It costs you to say goodbye, but it should cost you more to wear out your soul and your energy in the wrong place, it costs you because you love him, but you should love you more to accept that it is over, it costs you to face the changes, but living in your comfort zone will cost you more when you wake up in a few years and discover that you are not happy with what you have achieved, I know it is difficult to start from scratch, it hurts when it does not work what we most want, but they are tests, tests to accept that life is unpredictable and that nothing is ours except our will, be in control of your thoughts, do not keep accumulating scenes of the past, do not doubt yourself, for fear of what will happen next life is lost, you do not lose it and if you feel you lost it, go find it, you are always in time, except when you make the same mistake a thousand times and you know you are wrong but you prefer to keep doing it just for not letting it go'
